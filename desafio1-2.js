ca76d0a29c64e02a49cfd8c570b946a0014bfce3
Thalita Freitas <thalita.freitas1415@hotmail.com>
2020-06-17 10:41:08 -0300

    Primeiro commit

---
+++ desafio1-2.js
@@ -0,0 +1,10 @@
+const Empresa = {
+
+nome : "Rocketseat" ,
+cor :"Roxo",
+foco :"Programação",
+endereço:"Rua Guilherme Gembala, Numero 260", 
+
+}
+console.log(`A empresa ${Empresa.nome} está localizada em: ${Empresa.endereço}`)
+
