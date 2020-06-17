921add1e3d92d11dcd4efcc91b52c2994f2e66d4
Thalita Freitas <thalita.freitas1415@hotmail.com>
2020-06-16 23:00:27 -0300

    Primeiro commit

---
+++ vet.js
@@ -0,0 +1,20 @@
+const programador ={
+ nome: "Thalita",
+ idade: 18,
+ tecnologias:[
+
+
+{ nome: 'C++', especialidade: 'Desktop' },
+{ nome: 'Python', especialidade: 'Data Science' },
+{ nome: 'JavaScript', especialidade: 'Web/Mobile' }
+
+]
+
+}
+console.log(`A usuária ${programador.nome} tem ${programador.idade} anos e usa a tecnologia ${programador.tecnologias[0].nome}, com especialidade em ${programador.tecnologias[0].especialidade}`)
+
+
+
+
+
+
